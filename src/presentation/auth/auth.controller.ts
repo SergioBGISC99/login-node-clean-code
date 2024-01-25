621a27a072afdbec732b490e0a5b753fe5431ce2
Sergio Barreras <sergiobg.isc@gmail.com>
2024-01-24 19:31:21 -0700

    dto de registrar usuario

--- src/presentation/auth/auth.controller.ts
+++ src/presentation/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
+import { RegisterUserDto } from "../../domain/dtos";
 
 export class AuthController {
   constructor() {}
 
   registerUser = (req: Request, res: Response) => {
-    res.json("registerUser controller");
+    const [error, registerDto] = RegisterUserDto.create(req.body);
+    if (error) return res.status(400).json({ error });
+
+    res.json(registerDto);
   };
   loginUser = (req: Request, res: Response) => {
     res.json("loginUser controller");
