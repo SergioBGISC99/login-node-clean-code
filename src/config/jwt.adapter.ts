55390e71f27f0e813edc0e5ac3b04ba355da8d68
Sergio Barreras <sergiobg.isc@gmail.com>
2024-01-25 20:27:06 -0700

    validación de JWT

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -8,7 +8,16 @@ export class JwtAdapter {
     return new Promise((resolve) => {
       jwt.sign(payload, "SEED", { expiresIn: duration }, (error, token) => {
         if (error) resolve(null);
-        else resolve(token!);
+        resolve(token!);
+      });
+    });
+  }
+
+  static validateToken<T>(token: string): Promise<T|null> {
+    return new Promise((resolve) => {
+      jwt.verify(token, "SEED", (error, decoded) => {
+        if (error) resolve(null);
+        resolve(decoded as T);
       });
     });
   }
