9bca79fd62166a188949ae5bf5f6708553cb6d6c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-01-25 20:35:22 -0700

    semilla de JWT

--- src/config/envs.adapter.ts
+++ src/config/envs.adapter.ts
@@ -5,4 +5,5 @@ export const envs = {
   PORT: get("PORT").required().asPortNumber(),
   MONGO_URL: get("MONGO_URL").required().asString(),
   MONGO_DBNAME: get("MONGO_DBNAME").required().asString(),
+  JWT_SEED: get("JWT_SEED").required().asString(),
 };
--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import { envs } from "./envs.adapter";
+
+const secret = envs.JWT_SEED;
 
 export class JwtAdapter {
   static async generateToken(
@@ -6,16 +9,16 @@ export class JwtAdapter {
     duration: string = "2h"
   ): Promise<string | null> {
     return new Promise((resolve) => {
-      jwt.sign(payload, "SEED", { expiresIn: duration }, (error, token) => {
+      jwt.sign(payload, secret, { expiresIn: duration }, (error, token) => {
         if (error) resolve(null);
         resolve(token!);
       });
     });
   }
 
-  static validateToken<T>(token: string): Promise<T|null> {
+  static validateToken<T>(token: string): Promise<T | null> {
     return new Promise((resolve) => {
-      jwt.verify(token, "SEED", (error, decoded) => {
+      jwt.verify(token, secret, (error, decoded) => {
         if (error) resolve(null);
         resolve(decoded as T);
       });
