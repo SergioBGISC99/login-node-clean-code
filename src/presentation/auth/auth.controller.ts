2fe3c7456cfae1517f2389ad4a16e5ba020c48bd
Sergio Barreras <sergiobg.isc@gmail.com>
2024-01-25 20:28:26 -0700

    controlador con metodo de getUsers y validación de token

--- src/presentation/auth/auth.controller.ts
+++ src/presentation/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { AuthRepository, CustomError, RegisterUserDto } from "../../domain";
 import { JwtAdapter } from "../../config";
+import { UserModel } from "../../data/mongodb";
 
 export class AuthController {
   constructor(private readonly authRepository: AuthRepository) {}
@@ -22,12 +23,24 @@ export class AuthController {
       .then(async (user) => {
         res.json({
           user,
-          token: await JwtAdapter.generateToken({ email: user.email }),
+          token: await JwtAdapter.generateToken({ id: user.id }),
         });
       })
       .catch((error) => this.handleError(error, res));
   };
+
   loginUser = (req: Request, res: Response) => {
     res.json("loginUser controller");
+  };
+
+  getUsers = (req: Request, res: Response) => {
+    UserModel.find()
+      .then((users) => {
+        res.json({
+          //users,
+          user: req.body.user,
+        });
+      })
+      .catch(() => res.status(500).json({ error: "Internar Server Error" }));
   };
 }
