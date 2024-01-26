dbdfcea1b8094bcf8d24937e1b5759f90447a029
Sergio Barreras <sergiobg.isc@gmail.com>
2024-01-25 20:58:26 -0700

    caso de uso para registro de usuario

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,5 +1,11 @@
 export * from "./datasources/auth.datasource";
+
 export * from "./dtos/auth/register-user.dto";
+
 export * from "./entities/user.entity";
+
 export * from "./errors/custom.error";
+
 export * from "./repositories/auth.repository";
+
+export * from "./use-cases/auth/register-user.use-case";
--- src/presentation/auth/auth.controller.ts
+++ src/presentation/auth/auth.controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { AuthRepository, CustomError, RegisterUserDto } from "../../domain";
+import {
+  AuthRepository,
+  CustomError,
+  RegisterUser,
+  RegisterUserDto,
+} from "../../domain";
 import { JwtAdapter } from "../../config";
 import { UserModel } from "../../data/mongodb";
 
@@ -18,14 +23,9 @@ export class AuthController {
     const [error, registerDto] = RegisterUserDto.create(req.body);
     if (error) return res.status(400).json({ error });
 
-    this.authRepository
-      .register(registerDto!)
-      .then(async (user) => {
-        res.json({
-          user,
-          token: await JwtAdapter.generateToken({ id: user.id }),
-        });
-      })
+    new RegisterUser(this.authRepository)
+      .execute(registerDto!)
+      .then((data) => res.json(data))
       .catch((error) => this.handleError(error, res));
   };
 
